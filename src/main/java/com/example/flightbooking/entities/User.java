142bd5ffdfec3001e4b78d5070e30510980891e8
Amyliamg <xl2901@nyu.edu>
2023-07-16 14:25:26 -0500

    Write the User entity

--- src/main/java/com/example/flightbooking/entities/User.java
+++ src/main/java/com/example/flightbooking/entities/User.java
@@ -1,12 +1,47 @@
-package com.example.flightbooking.entities;
+package com.example.flight.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import com.example.flightbooking.entities.AbstractEntity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
 @Entity
-@Table(name = "user")
-public class User {
+@Table(name = "USER")
+public class User extends AbstractEntity {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
 
+    public String getFirstName() {
+        return firstName;
+    }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
